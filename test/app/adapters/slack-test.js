8ccb4d89bf2799f715540452960abf978064ff9b
Kurt Moeller <kurt.moeller@teamaol.com>
2016-05-19 14:32:53 -0700

    adding gulp

--- test/app/adapters/slack-test.js
+++ test/app/adapters/slack-test.js
@@ -1,1 +1,8 @@
+const sinon = require('sinon');
+require('mocha-generators').install();
+const chai = require('chai');
+const expect = chai.expect; // jshint ignore:line
+const should = chai.should();
+
 const Slack = require('../../adapters/slack');
+
