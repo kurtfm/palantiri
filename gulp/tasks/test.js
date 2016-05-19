4e332c7fdf6442e83e90a6a0554f46a3fe15ef61
Kurt Moeller <kurt.moeller@teamaol.com>
2016-05-19 14:35:51 -0700

    clean unused files for now

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
+process.env.NODE_ENV = 'test';
+
 gulp.task('test', function () {
 	return gulp.src('test/*-test.js', {read: false})
 		.pipe(mocha({timeout:15000}))
